3c899b0ef908d448b883a5221e4489655264aa0b
Stéphanie AMANS <stephanie-amans_student2021@wilder.school>
2021-12-01 09:25:47 +0100

    doctrine data stored

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,12 +1,13 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
     /**
-    * @Route("/program/", name="program_")
+    * @Route("/program", name="program_")
     */
 Class ProgramController extends AbstractController
 
@@ -16,18 +17,30 @@ Class ProgramController extends AbstractController
      */
     public function index(): Response
     {
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Séries',
+            'programs' => $programs,
         ]);
     }
 
     /**
-     * @Route("{id}/", name="show", methods="GET", requirements={"id"="\d{1,}"})
+     * @Route("/{id}/", name="show", methods="GET", requirements={"id"="\d{1,}"})
      */
     public function show(int $id): Response
     {
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $id]);
+
+        if(!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.'found in program\'s table.'
+            );
+        }
         return $this->render('program/show.html.twig', [
-            'id' => $id,
+            'program' => $program,
         ]);
     }
 }
